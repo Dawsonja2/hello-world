5baaa67b1d040782038e4b59a9ecd245cc2f246e
Dawsonja2 <59925930+Dawsonja2@users.noreply.github.com>
2020-01-15 10:26:58 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/********************
+* Hello.java
+*********************
+*  Jared Dawson
+* January 15, 2020
+* CMSC 255 Section 003
+*********************/
+
+public class Hello {
+    
+    public static void main(String[] args) {
+    
+    	System.out.println("<Jared Dawson>") ;
+		System.out.println("<Hello.java>") ;
+		System.out.println("<Java>") ;
+		System.out.println("<January 15, 2019>") ;
+		System.out.println("<CMSC 255 Section 003>") ;
+		System.out.println() ;
+    }	
+}
